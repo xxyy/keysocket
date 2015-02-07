442f3c663f6ecf04725c7b2de41a37b344b304ed
xxyy <devnull@nowak-at.net>
2015-02-07 02:03:35 +0100

    Fixed prev/next buttons being inversed
    It's 2am, okay?

--- extension/keysocket-youtube.js
+++ extension/keysocket-youtube.js
@@ -1,13 +1,13 @@
 function onKeyPress(key) {
     if(key === NEXT) {
-        var nextButton = document.querySelector('.ytp-button-prev'); 
+        var nextButton = document.querySelector('.ytp-button-next'); 
         simulateClick(nextButton);
     } else if(key === PLAY) {
         if(!simulateClick(document.querySelector('.ytp-button-play'))) {
             simulateClick(document.querySelector('.ytp-button-pause'));
         } 
     } else if(key === PREV) {
-        var backButton = document.querySelector('.ytp-button-next');
+        var backButton = document.querySelector('.ytp-button-prev');
         simulateClick(backButton);
     }
 }
