8ca1bbae8c939ee67830ab6d6f0b3d61bd5240d6
xxyy <devnull@nowak-at.net>
2015-02-07 01:44:43 +0100

    Make YouTube socket work (only) for HTML5

--- extension/keysocket-youtube.js
+++ extension/keysocket-youtube.js
@@ -1,29 +1,13 @@
 function onKeyPress(key) {
     if(key === NEXT) {
-        var nextButton = document.querySelector('.yt-uix-button-icon-playlist-bar-next,.yt-uix-button-icon-watch-appbar-play-next'); 
+        var nextButton = document.querySelector('.ytp-button-prev'); 
         simulateClick(nextButton);
     } else if(key === PLAY) {
-        var video = document.querySelector('#movie_player');
-        
-        // The extension has no access to this function when using the HTML-5 <video>-player
-        // but we can still access the controls on the <video>-element itself
-        if(!video.getPlayerState) {
-            video = document.querySelector('#movie_player video'); // HTML-5 <video>-player
-        
-            if(video.paused) {
-                video.play();
-            } else {
-                video.pause();
-            }
-        } else {
-            if(video.getPlayerState() === 2) {
-                video.playVideo();
-            } else {
-                video.pauseVideo();
-            }
-        }
+        if(!simulateClick(document.querySelector('.ytp-button-play'))) {
+            simulateClick(document.querySelector('.ytp-button-pause'));
+        } 
     } else if(key === PREV) {
-        var backButton = document.querySelector('.yt-uix-button-icon-playlist-bar-prev,.yt-uix-button-icon-watch-appbar-play-prev');
+        var backButton = document.querySelector('.ytp-button-next');
         simulateClick(backButton);
     }
 }
